8742867b07fda017a5698b618a29d32ab7f51d3f
Your Name <you@example.com>
2018-06-27 13:28:08 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,46 +1,10 @@
 def reformat_languages(languages)
   # your code here
-  # iterate through languages once, get oo and functional
-
-  languages = {
-    :oo => {
-      :ruby => {
-        :type => "interpreted"
-      },
-      :javascript => {
-        :type => "interpreted"
-      },
-      :python => {
-        :type => "interpreted"
-      },
-      :java => {
-        :type => "compiled"
-      }
-    },
-    :functional => {
-      :clojure => {
-        :type => "compiled"
-      },
-      :erlang => {
-        :type => "compiled"
-      },
-      :scala => {
-        :type => "compiled"
-      },
-      :javascript => {
-        :type => "interpreted"
-      }
-
-    }
-  }
-
-  new_hash = {}
-  languages.collect do |type_language, info|
-  # iterate through each type, get languages
-  array = []
-  array << type_language
+new_hash = {}
+languages.collect do |type_language, info|
+array = []
+array << type_language
     info.collect do |key, value|
-  # get the language
     new_hash[key] = value
     new_hash[key][:style] = array
     end
